03336a85b18eefa36d7ed4ea733ea8eee8bf9db1
TG <trygod.nwakwasi@decagon.dev>
2022-12-08 03:50:00 +0100

    created the Ranking class

---
+++ src/main/java/com/lyrasolarpokerhands/Ranking.java
@@ -0,0 +1,14 @@
+package com.lyrasolarpokerhands;
+
+public enum Ranking {
+    HIGH_CARD,
+    PAIR,
+    TWO_PAIR,
+    TRIPS,
+    STRAIGHT,
+    FLUSH,
+    FULL_HOUSE,
+    QUADS,
+    STRAIGHT_FLUSH,
+    ROYAL_FLUSH
+}
