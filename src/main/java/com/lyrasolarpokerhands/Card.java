2c3516f63bbd053b89314b1ded5ca2d7a0e985b6
TG <trygod.nwakwasi@decagon.dev>
2022-12-08 01:34:14 +0100

    created the Card class

---
+++ src/main/java/com/lyrasolarpokerhands/Card.java
@@ -0,0 +1,34 @@
+package com.lyrasolarpokerhands;
+
+public class Card implements Comparable<Card>{
+
+    private final Value value;
+    private final Suit suit;
+
+    public Card(Value value, Suit suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public Card(String s) {
+        this(Value.of(s.charAt(0)), Suit.of(s.charAt(1)));
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    @Override
+    public int compareTo(Card o) {
+        return value.compareTo(o.value);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%s", value.getValueChar(), suit.getSuitChar());
+    }
+}
